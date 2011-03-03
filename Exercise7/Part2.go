6eb1f2664808c4bfbc3673a57a8328ed0627941e
Marius Ludvigsen <marius.ludvigsen@gmail.com>
2011-03-03 12:22:46 +0100

    Commit again

--- Exercise7/Part2.go
+++ Exercise7/Part2.go
@@ -1,8 +1,27 @@
 package main
 import (
 	"fmt"
+//	"sync"	
 )
 
+var ch channel
+var srvInt int = 0
+
+func server(newint channel int){
+	srvInt <- newint
+	fmt.Printf("Int has changed, it is now: %d", srvInt)
+}
+
+func client(ch channel int){
+	while(true){
+		var temp int <- ch
+		ch <- temp+3
+	    sleep(1)
+	}
+}
+
 func main(){
-	fmt.Printf("ASDF!\n")
+	go server(channel)
+	go client(channel)
+	go client(channel)	
 }
